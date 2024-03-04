9617e8e7a3030c8339d173b4fc89bc7f671f9a92
КуклевГА <gorg>
2024-03-04 11:18:58 +0300

    Change output

--- matModelirovanie/SimpleTable.cs
+++ matModelirovanie/SimpleTable.cs
@@ -8,50 +8,51 @@ namespace matModelirovanie
 {
     internal class SimpleTable
     {
-        int[,] simpleTable = { { 2, -1, 1, 1, 1},{-4, 2, -1, 1, 2 },{ 3, 0, 1, 1, 5} };
+        //= { { 2, -1, 1, 1, 1},{-4, 2, -1, 1, 2 },{ 3, 0, 1, 1, 5} }
+        int[,] simpleTable ;
         int[,] tableAll;
         public SimpleTable()
         {
-            //InputSystem();
+            InputSystem();
         }
         public void InputSystem()
         {
-            //    Console.WriteLine("Введите количество уравнений в системе");
-            //    int countYrav = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine("Введите количество основых переменных");
-            //    int countXValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество уравнений в системе");
+            int countYrav = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество основых переменных");
+            int countXValue = Convert.ToInt32(Console.ReadLine());
 
-            //    int countI = 1;
-            //    simpleTable = new int[countYrav, countXValue + 2];
+            int countI = 1;
+            simpleTable = new int[countYrav, countXValue + 2];
 
-            //    for (int i = 0; i < simpleTable.GetLength(0); i++)
-            //    {
+            for (int i = 0; i < simpleTable.GetLength(0); i++)
+            {
 
-            //        int countJ = 1;
-            //        for (int j = 0; j < simpleTable.GetLength(1) - 2; j++)
-            //        {
-            //            Console.WriteLine($"Введите значение коэффициент переменной x{countJ++} уравнения {countI}");
-            //            simpleTable[i, j] = Convert.ToInt32(Console.ReadLine());
-            //        }
-            //        countI++;
-            //    }
+                int countJ = 1;
+                for (int j = 0; j < simpleTable.GetLength(1) - 2; j++)
+                {
+                    Console.WriteLine($"Введите значение коэффициент переменной x{countJ++} уравнения {countI}");
+                    simpleTable[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                countI++;
+            }
 
-            //    for (int i = 0; i < simpleTable.GetLength(0); i++)
-            //    {
-            //        simpleTable[i, simpleTable.GetLength(1) - 2] = 1;
-            //    }
+            for (int i = 0; i < simpleTable.GetLength(0); i++)
+            {
+                simpleTable[i, simpleTable.GetLength(1) - 2] = 1;
+            }
 
-            //    countI = 1;
-            //    for (int i = 0; i < simpleTable.GetLength(0); i++)
-            //    {
-            //        Console.WriteLine($"Введите значение уравнения {countI}");
-            //        simpleTable[i, simpleTable.GetLength(1) - 1] = Convert.ToInt32(Console.ReadLine());
-            //    }
+            countI = 1;
+            for (int i = 0; i < simpleTable.GetLength(0); i++)
+            {
+                Console.WriteLine($"Введите значение уравнения {countI}");
+                simpleTable[i, simpleTable.GetLength(1) - 1] = Convert.ToInt32(Console.ReadLine());
+            }
             OutputSystem();
         }
         public void OutputSystem()
         {
-            int countBasicVariables = simpleTable.GetLength(0) + 1;
+            int countBasicVariables = (simpleTable.GetLength(1) - 2) + 1;
 
             Console.WriteLine($"Система уравнений:\n");
             for (int i = 0; i < simpleTable.GetLength(0); i++)
